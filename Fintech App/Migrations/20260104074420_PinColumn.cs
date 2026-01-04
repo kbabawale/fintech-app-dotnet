192ca116589573d4dcb1f354060a390c61892230
kbabawale <kolapobabawale@gmail.com>
2026-01-04 22:46:51 +0100

    introduced `Pin` column in Accounts table.

---
+++ Fintech App/Migrations/20260104074420_PinColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fintech_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class PinColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Pin",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pin",
+                table: "Accounts");
+        }
+    }
+}
