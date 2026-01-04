c4c33c4bbee1bbe6c918615104f445cb9d52cca1
kbabawale <kolapobabawale@gmail.com>
2026-01-04 22:48:18 +0100

    Ability to set defaults when necessary

--- Fintech App/Model/Domain/Account.cs
+++ Fintech App/Model/Domain/Account.cs
@@ -5,14 +5,15 @@ using Fintech_App.Model.Others;
 namespace Fintech_App.Model.Domain
 {
     [Table("Accounts")]
-    public class Account : BaseSchema
+    public class Account(int AccountNumber, Guid userId, string Pin, Currency currency = Currency.NGN) : BaseSchema
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; set; } = AccountNumber;
+        public string Pin { get; set; } = Pin;
         public decimal Balance { get; set; } = 0;
-        public Currency Currency { get; set; } = Currency.NGN;
-        public Guid UserId { get; set; }
-        public User? User { get; set; }
+        public Currency Currency { get; set; } = currency;
+        public Guid UserId { get; set; } = userId;
+        public User User { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
 
--- Fintech App/Model/Domain/User.cs
+++ Fintech App/Model/Domain/User.cs
@@ -5,13 +5,13 @@ using Fintech_App.Model.Others;
 namespace Fintech_App.Model.Domain
 {
     [Table("Users")]
-    public class User : BaseSchema
+    public class User(string FirstName, string LastName, string Email, string PhoneNumber) : BaseSchema
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; } = String.Empty;
-        public string LastName { get; set; } = String.Empty;
-        public string Email { get; set; } = String.Empty;
-        public string PhoneNumber { get; set; } = String.Empty;
+        public string FirstName { get; set; } = FirstName;
+        public string LastName { get; set; } = LastName;
+        public string Email { get; set; } = Email;
+        public string PhoneNumber { get; set; } = PhoneNumber;
         public ICollection<Account> Accounts { get; set; } = [];
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
